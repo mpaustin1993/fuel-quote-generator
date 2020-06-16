2c06f6a5281e176c6def0c5b4e7513b42b4f772a
Michael Austin <mpaustin1993@gmail.com>
2020-06-16 14:26:21 -0500

    made fuelquote.php & spacing below submit bar of all other pages

--- index.php
+++ index.php
@@ -1,26 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<title>Login V1</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="css/util.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 </head>
+
 <body>
 
 	<div class="limiter">
@@ -35,7 +37,7 @@
 						Member Login
 					</span>
 
-					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+					<div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
 						<input class="input100" type="text" name="email" placeholder="Email">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
@@ -45,7 +47,7 @@
 
 
 
-					<div class="wrap-input100 validate-input" data-validate = "Password is required">
+					<div class="wrap-input100 validate-input" data-validate="Password is required">
 						<input class="input100" type="password" name="pass" placeholder="Password">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
@@ -66,14 +68,18 @@
 						<a class="txt2" href="forgotpassword.php">
 							Username / Password?
 						</a>
-					</div>
-
-					<div class="text-center p-t-136">
+						<br>
 						<a class="txt2" href="signup.php">
 							Create your Account
 							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
 						</a>
+						<br>
+						<a class="txt2" href="fuelquote.php">
+							Get a Fuel Quote
+							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+						</a>
 					</div>
+
 				</form>
 			</div>
 		</div>
@@ -82,22 +88,23 @@
 
 
 
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/bootstrap/js/popper.js"></script>
 	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/tilt/tilt.jquery.min.js"></script>
-	<script >
+	<script>
 		$('.js-tilt').tilt({
 			scale: 1.1
 		})
 	</script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="js/main.js"></script>
 
 </body>
+
 </html>
--- signup.php
+++ signup.php
@@ -1,26 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<title>Member Signup</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="css/util.css">
 	<link rel="stylesheet" type="text/css" href="css/override.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 </head>
+
 <body>
 
 	<div class="limiter">
@@ -36,28 +38,28 @@
 						Member Signup
 					</span>
 
-          <div class="row">
-            <div class="col-sm-6">
-              <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-    						<input class="input100" type="text" name="userfirst" placeholder="First Name">
-    						<span class="focus-input100"></span>
-    						<span class="symbol-input100">
-    							<i class="fa fa-address-card" aria-hidden="true"></i>
-    						</span>
-    					</div>
-            </div>
-            <div class="col-sm-6">
-              <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-    						<input class="input100" type="text" name="userlast" placeholder="Last Name">
-    						<span class="focus-input100"></span>
-    						<span class="symbol-input100">
-    							<i class="fa fa-address-card" aria-hidden="true"></i>
-    						</span>
-    					</div>
-            </div>
-          </div>
+					<div class="row">
+						<div class="col-sm-6">
+							<div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+								<input class="input100" type="text" name="userfirst" placeholder="First Name">
+								<span class="focus-input100"></span>
+								<span class="symbol-input100">
+									<i class="fa fa-address-card" aria-hidden="true"></i>
+								</span>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+								<input class="input100" type="text" name="userlast" placeholder="Last Name">
+								<span class="focus-input100"></span>
+								<span class="symbol-input100">
+									<i class="fa fa-address-card" aria-hidden="true"></i>
+								</span>
+							</div>
+						</div>
+					</div>
 
-          <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+					<div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
 						<input class="input100" type="text" name="address1" placeholder="Address">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
@@ -65,7 +67,7 @@
 						</span>
 					</div>
 
-          <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+					<div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
 						<input class="input100" type="text" name="address2" placeholder="Address">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
@@ -73,36 +75,36 @@
 						</span>
 					</div>
 
-          <div class="row">
-            <div class="col-sm-6">
-              <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-    						<input class="input100" type="text" name="city" placeholder="City">
-    						<span class="focus-input100"></span>
-    						<span class="symbol-input100">
-    							<i class="fa fa-address-card" aria-hidden="true"></i>
-    						</span>
-    					</div>
-            </div>
-            <div class="col-sm-6">
-              <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-    						<select class="input100" type="text" name="state" placeholder="State">
-                  <option value="TX">TX</option>
-                  <option value="AZ">AZ</option>
-                  <option value="MS">MS</option>
-                  <option value="TE">TE</option>
-                  </select>
-    						<span class="focus-input100"></span>
-    						<span class="symbol-input100">
-    							<i class="fas fa-map-signs" aria-hidden="true"></i>
-    						</span>
-    					</div>
+					<div class="row">
+						<div class="col-sm-6">
+							<div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+								<input class="input100" type="text" name="city" placeholder="City">
+								<span class="focus-input100"></span>
+								<span class="symbol-input100">
+									<i class="fa fa-address-card" aria-hidden="true"></i>
+								</span>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+								<select class="input100" type="text" name="state" placeholder="State">
+									<option value="TX">TX</option>
+									<option value="AZ">AZ</option>
+									<option value="MS">MS</option>
+									<option value="TE">TE</option>
+								</select>
+								<span class="focus-input100"></span>
+								<span class="symbol-input100">
+									<i class="fas fa-map-signs" aria-hidden="true"></i>
+								</span>
+							</div>
 
 
 
-            </div>
-          </div>
+						</div>
+					</div>
 
-					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+					<div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
 						<input class="input100" type="text" name="email" placeholder="Email">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
@@ -110,15 +112,15 @@
 						</span>
 					</div>
 
-          <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
-            <input class="input100" type="text" name="username" placeholder="Username">
-            <span class="focus-input100"></span>
-            <span class="symbol-input100">
-              <i class="fa fa-user" aria-hidden="true"></i>
-            </span>
-          </div>
+					<div class="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
+						<input class="input100" type="text" name="username" placeholder="Username">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-user" aria-hidden="true"></i>
+						</span>
+					</div>
 
-					<div class="wrap-input100 validate-input" data-validate = "Password is required">
+					<div class="wrap-input100 validate-input" data-validate="Password is required">
 						<input class="input100" type="password" name="pass" placeholder="Password">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
@@ -126,7 +128,7 @@
 						</span>
 					</div>
 
-          <div class="wrap-input100 validate-input" data-validate = "Password is required">
+					<div class="wrap-input100 validate-input" data-validate="Password is required">
 						<input class="input100" type="password" name="pass-confirm" placeholder="Password Confirm">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
@@ -147,14 +149,13 @@
 						<a class="txt2" href="forgotpassword.php">
 							Username / Password?
 						</a>
-					</div>
-
-					<div class="text-center p-t-136">
+						<br>
 						<a class="txt2" href="index.php">
 							Already have an account? Log in
 							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
 						</a>
 					</div>
+
 				</form>
 			</div>
 		</div>
@@ -163,22 +164,23 @@
 
 
 
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/bootstrap/js/popper.js"></script>
 	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="vendor/tilt/tilt.jquery.min.js"></script>
-	<script >
+	<script>
 		$('.js-tilt').tilt({
 			scale: 1.1
 		})
 	</script>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<script src="js/main.js"></script>
 
 </body>
+
 </html>
