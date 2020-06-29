4dce5ce0045f1220b4ce6e9d22e79eabf20dc661
hbuiOnline <61067968+hbuiOnline@users.noreply.github.com>
2020-06-29 17:45:19 -0500

    error between branches

--- classes/ClientProfileView.class.php
+++ classes/ClientProfileView.class.php
@@ -3,11 +3,8 @@
 class ClientProfileView extends ClientProfile
 {
 
-<<<<<<< HEAD
-  public function clientDataShow()
-=======
+
   public function getClientData()
->>>>>>> e25ac94f4bb269b3bff78104426af642da632e6a
   {
     $result = array();
     $result = $this->clientProfileData();
