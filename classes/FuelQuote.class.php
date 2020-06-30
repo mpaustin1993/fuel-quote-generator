6e3e46c23882d37feaa56bb20e16d2a7c8f3d471
Michael Austin <mpaustin1993@gmail.com>
2020-06-29 19:34:29 -0500

    changed profile mgmt design

--- classes/FuelQuote.class.php
+++ classes/FuelQuote.class.php
@@ -2,14 +2,14 @@
 
 session_start();
 
-class FuelQuote extends Dbh {
+class FuelQuote extends Dbh
+{
 
-  protected function fuelQuoteInput($quoteClientId, $quoteGallon, $quoteDeliverAdr,$quoteState, $quoteZipcode, $quoteDeliverDate){
-
+  protected function fuelQuoteInput($quoteClientId, $quoteGallon, $quoteAddress, $quoteCity, $quoteState, $quoteZip, $quoteDeliveryDate)
+  {
   }
 
-  protected function fuelQuoteData(){ //Extracting data from the DB , including quoteHistory and currentQuote
-
+  protected function fuelQuoteData()
+  { //Extracting data from the DB , including quoteHistory and currentQuote
   }
-
 }
